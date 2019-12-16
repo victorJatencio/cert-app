15b5b3f4ffe8ebc80448243a04e523d9dc398e7e
Victor Atencio <victoratencio@Victors-MBP.fios-router.home>
2019-12-15 20:10:55 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import BatchPage from "./BatchPage.component";
+
+function App() {
+  return (
+    <div>
+      <BatchPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BatchPage.component.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BatchPage = () => (
+  <div className="batchpage">
+    <div>Batch Page</div>
+  </div>
+);
+
+export default BatchPage;
